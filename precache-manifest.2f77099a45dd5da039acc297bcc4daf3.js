e25ff2b60ce8bbe29bb746de75188e32a48f1b37
Big 9 <big9@MacBook-Pro.local>
2020-09-16 01:44:32 +0800

    Updates

---
+++ precache-manifest.2f77099a45dd5da039acc297bcc4daf3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1236ec1f5d9eb14ec213bc58e60301ea",
+    "url": "/bchat/index.html"
+  },
+  {
+    "revision": "4abedcc2c91698e3704b",
+    "url": "/bchat/static/css/main.e52c8f9c.chunk.css"
+  },
+  {
+    "revision": "8e42292c54899722ddf5",
+    "url": "/bchat/static/js/2.7a2f53bd.chunk.js"
+  },
+  {
+    "revision": "97c0b5374351518bd0674c0fa3f02654",
+    "url": "/bchat/static/js/2.7a2f53bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4abedcc2c91698e3704b",
+    "url": "/bchat/static/js/main.805b3329.chunk.js"
+  },
+  {
+    "revision": "37f39f4e6e3f33d7aead",
+    "url": "/bchat/static/js/runtime-main.fecc921d.js"
+  },
+  {
+    "revision": "9299706f23346da2776f4aef1e52996a",
+    "url": "/bchat/static/media/ic_wave_hand.9299706f.png"
+  },
+  {
+    "revision": "477fc8ddd40d200fe8f2635b5151a873",
+    "url": "/bchat/static/media/mimi1.477fc8dd.gif"
+  },
+  {
+    "revision": "82a783353a27abe59feafd40bc686af1",
+    "url": "/bchat/static/media/mimi2.82a78335.gif"
+  },
+  {
+    "revision": "477fc8ddd40d200fe8f2635b5151a873",
+    "url": "/bchat/static/media/mimi3.477fc8dd.gif"
+  },
+  {
+    "revision": "6b65742393c65511ba223ec388c85ecc",
+    "url": "/bchat/static/media/mimi4.6b657423.gif"
+  },
+  {
+    "revision": "609127cb5129f8e218fb57f059497c87",
+    "url": "/bchat/static/media/mimi5.609127cb.gif"
+  },
+  {
+    "revision": "1ce35a6b3c21cc308a7683dfbea81c63",
+    "url": "/bchat/static/media/mimi6.1ce35a6b.gif"
+  },
+  {
+    "revision": "ace546bd302229af3a0c7e7221d2dc10",
+    "url": "/bchat/static/media/mimi7.ace546bd.gif"
+  },
+  {
+    "revision": "6646a3dec849498313a17c7de5d2ea5c",
+    "url": "/bchat/static/media/mimi8.6646a3de.gif"
+  },
+  {
+    "revision": "b5ec7c32285b24c09eca4646a4511518",
+    "url": "/bchat/static/media/mimi9.b5ec7c32.gif"
+  }
+]);
